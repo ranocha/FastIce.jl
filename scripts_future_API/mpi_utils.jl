0d53756c77511a60ed5e6f1272c0f1c766b0c94d
Ludovic Raess <ludovic.rass@gmail.com>
2023-09-07 01:31:04 +0300

    Add gather

--- scripts_future_API/mpi_utils.jl
+++ scripts_future_API/mpi_utils.jl
@@ -25,3 +25,31 @@ function split_ndrange(ndrange::CartesianIndices{N},ndwidth::NTuple{N,<:Integer}
         end
     end
 end
+
+function gather!(dst, src, comm; root=0)
+    dims, _, _ = MPI.Cart_get(comm)
+    dims = Tuple(dims)
+    if MPI.Comm_rank(comm) == root
+        # make subtype for gather
+        subtype = MPI.Types.create_subarray(size(dst), size(src), (0, 0), MPI.Datatype(eltype(dst)))
+        subtype = MPI.Types.create_resized(subtype, 0, size(src, 1) * Base.elsize(dst))
+        MPI.Types.commit!(subtype)
+        # make VBuffer for collective communication
+        counts = fill(Cint(1), dims)
+        displs = similar(counts)
+        d = zero(Cint)
+        for j in 1:dims[2]
+            for i in 1:dims[1]
+                displs[i, j] = d
+                d += 1
+            end
+            d += (size(src, 2) - 1) * dims[2]
+        end
+        # transpose displs as cartesian communicator is row-major
+        recvbuf = MPI.VBuffer(dst, vec(counts), vec(displs'), subtype)
+        MPI.Gatherv!(src, recvbuf, comm; root)
+    else
+        MPI.Gatherv!(src, nothing, comm; root)
+    end
+    return
+end
